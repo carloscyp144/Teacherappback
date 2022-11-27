2a096120f7e34ebbdadf9e0d26c2e5531f079946
Mariano Jiménez <mjifris@hotmail.com>
2022-11-27 23:02:30 +0100

    Faltaba use cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const apiDocsRouter = require('./routes/api-docs');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
